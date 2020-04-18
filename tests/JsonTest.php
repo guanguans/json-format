8a62f4de10b0cf1c1b9f63b9d8366cb14b98e3d9
guanguans <yzmguanguan@gmail.com>
2020-04-18 16:12:48 +0800

    Fix testing

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -18,7 +18,7 @@ class JsonTest extends TestCase
     public function testInvalidString()
     {
         $array = ['mac-array'];
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(get_class(new InvalidArgumentException()));
         $this->expectExceptionMessage(sprintf('Parameter type is not valid string: %s', gettype($array)));
         Json::format($array);
         $this->fail('Faild to assert format method throw exception with invalid argument.');
@@ -27,7 +27,7 @@ class JsonTest extends TestCase
     public function testInvalidJson()
     {
         $string = 'mac-string';
-        $this->expectException(InvalidArgumentException::class);
+        $this->expectException(get_class(new InvalidArgumentException()));
         $this->expectExceptionMessage(sprintf('Parameter is not valid JSON: %s', $string));
         Json::format($string);
         $this->fail('Faild to assert format method throw exception with invalid argument.');

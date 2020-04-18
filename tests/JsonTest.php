2510e7faf1ad6d07c1104b0b8534bf8d6d1d3143
guanguans <yzmguanguan@gmail.com>
2020-04-18 15:46:43 +0800

    Add parameter exception testing

--- tests/JsonTest.php
+++ tests/JsonTest.php
@@ -10,10 +10,29 @@
 
 namespace Guanguans\Tests;
 
+use Guanguans\JsonFormat\InvalidArgumentException;
 use Guanguans\JsonFormat\Json;
 
 class JsonTest extends TestCase
 {
+    public function testInvalidString()
+    {
+        $array = ['mac-array'];
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(sprintf('Parameter type is not valid string: %s', gettype($array)));
+        Json::format($array);
+        $this->fail('Faild to assert format method throw exception with invalid argument.');
+    }
+
+    public function testInvalidJson()
+    {
+        $string = 'mac-string';
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(sprintf('Parameter is not valid JSON: %s', $string));
+        Json::format($string);
+        $this->fail('Faild to assert format method throw exception with invalid argument.');
+    }
+
     public function testFormat()
     {
         $json = <<<'JSON'

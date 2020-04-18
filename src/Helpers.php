6445871e952125bf60987cc73bd46a45554ee48d
guanguans <yzmguanguan@gmail.com>
2020-04-18 14:43:08 +0800

    Add `json_format` function

---
+++ src/Helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the guanguans/json-format.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+use Guanguans\JsonFormat\Json;
+
+if (!function_exists('json_format')) {
+    /**
+     * @param $json
+     *
+     * @return false|string
+     *
+     * @throws \Guanguans\JsonFormat\InvalidArgumentException
+     */
+    function json_format($json)
+    {
+        return Json::format($json);
+    }
+}
